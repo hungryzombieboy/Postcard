eb18ed92cab0f436868996d2238bff1e83d20f80
hungryzombieboy <vidal_1986@yahoo.com>
2015-05-01 16:32:07 -0400

    second comment for testing

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //This is a comment for testing purposes
     }
 
     
